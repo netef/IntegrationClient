346bfdbcdd803b35f7292ec6b11fb21fbb87e627
netef <38144727+netef@users.noreply.github.com>
2019-05-28 14:41:35 +0300

    added more screens

---
+++ app/src/main/java/com/Integration/client/AddNewShowActivity.java
@@ -0,0 +1,13 @@
+package com.Integration.client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_show);
+    }
+}

ae5e25ea7ed622583bf8e355486833d9ff69b59d
netef <netef10@gmail.com>
2019-06-02 01:32:05 +0300

    updated drawble names

--- app/src/main/java/com/Integration/client/MainActivity.java
+++ app/src/main/java/com/Integration/client/MainActivity.java
@@ -2,6 +2,17 @@ package com.Integration.client;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.textclassifier.TextLinks;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //change the port accordingly
+        String URL = "http://localhost: + port + /smartspace/";
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response:" , response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Error:" , error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
     }
 }
